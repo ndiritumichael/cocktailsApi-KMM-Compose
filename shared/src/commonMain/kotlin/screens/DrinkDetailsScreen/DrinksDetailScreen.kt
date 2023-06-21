a8f342abe2c8c8f90547a446a5144bb90bcf64cf
ndiritumichael <ndiritumichael5@gmail.com>
2023-06-21 23:03:28 +0300

    added compononents for the detail screen

--- shared/src/commonMain/kotlin/screens/DrinkDetailsScreen/DrinksDetailScreen.kt
+++ shared/src/commonMain/kotlin/screens/DrinkDetailsScreen/DrinksDetailScreen.kt
@@ -1,27 +1,96 @@
 package screens.DrinkDetailsScreen
 
+import AsyncImage
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import presentation.DrinksSearchPresenter
 
 data class DrinksDetailScreen(val drinkId: String) : Screen, KoinComponent {
-    val presenter: DrinksSearchPresenter by inject()
-
+    private val presenter: DrinksSearchPresenter by inject()
 
     @Composable
     override fun Content() {
-
-        LaunchedEffect(true){
-            presenter.
+        val drinkState = presenter.drinkDetailState.collectAsState().value
+        val navigator = LocalNavigator.currentOrThrow
+        LaunchedEffect(true) {
+            presenter.getDrinkDetails(drinkId)
+            // presenter.
         }
-Scaffold {
+        Scaffold {
+            Box(
+                modifier = Modifier.fillMaxSize().padding(top = 100.dp),
+                contentAlignment = Alignment.TopCenter,
+            ) {
+                AnimatedVisibility(drinkState.isLoading) {
+                    CircularProgressIndicator(modifier = Modifier.size(50.dp))
+                }
+                drinkState.errorMessage?.let { error ->
+                    Column {
+                        Text(error, color = Color.Red)
+                        Button({ presenter.getDrinkDetails(drinkId) }) {
+                            Text("Retry")
+                        }
+                    }
+                }
+            }
+            drinkState.data?.let { drink ->
 
+                LazyColumn {
+                    item {
+                        AsyncImage(
+                            drink.drinkImage,
+                            modifier = Modifier.fillMaxWidth(),
+                            contentScale = ContentScale.FillWidth,
+                        )
+                    }
+                }
+            }
+        }
+        Box(modifier = Modifier.fillMaxSize()) {
+            Row(modifier = Modifier.fillMaxWidth().align(Alignment.TopCenter), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                IconButton(
+                    { navigator.pop() },
+                    modifier = Modifier.clip(CircleShape)
+                        .background(Color.LightGray)
+                ) {
+                    Icon(Icons.Filled.ArrowBack, null, tint = Color.Black)
+                }
 
-
-}
+                Text(drinkState.data?.name ?: "", fontWeight = FontWeight.Bold)
+            }
+        }
     }
 }
