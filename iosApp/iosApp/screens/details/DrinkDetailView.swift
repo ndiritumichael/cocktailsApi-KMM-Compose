61bca10434fb6c422d007e7823dbeb26d7dd820d
ndiritumichael <ndiritumichael5@gmail.com>
2023-07-02 22:52:55 +0300

    minor UI restructure

--- iosApp/iosApp/screens/details/DrinkDetailView.swift
+++ iosApp/iosApp/screens/details/DrinkDetailView.swift
@@ -1,22 +1,21 @@
 //
-//  HomeScreen.swift
+//  SwiftUIView.swift
 //  iosApp
 //
-//  Created by Michael Ndiritu on 01/07/2023.
+//  Created by Michael Ndiritu on 02/07/2023.
 //  Copyright © 2023 orgName. All rights reserved.
 //
 
 import SwiftUI
-import shared
 
-struct HomeScreen: View {
+struct DrinkDetailView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct HomeScreen_Previews: PreviewProvider {
+struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeScreen()
+       DrinkDetailView()
     }
 }
