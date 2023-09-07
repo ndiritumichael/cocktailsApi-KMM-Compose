333427dfef2b05a282983c15e0db682e377467ba
ndiritumichael <ndiritumichael5@gmail.com>
2023-09-07 23:52:40 +0300

    code refactor.

--- shared/src/commonMain/kotlin/utils/Utils.kt
+++ shared/src/commonMain/kotlin/utils/Utils.kt
@@ -1,11 +1,14 @@
 package utils
 
-fun <T,Y> Result<T>.toModelResult(converter: (T) ->Y ):Result<Y>{
-     this.onSuccess {
-       return  Result.success(converter(it))
-
+fun <T, Y> Result<T>.toModelResult(converter: (T) -> Y): Result<Y> {
+    this.onSuccess {
+        return Result.success(converter(it))
     }.onFailure {
         return Result.failure<Y>(it)
-     }
+    }
     return Result.failure<Y>(Exception("Something went wrong"))
+}
+
+fun Throwable.getMessage(): String {
+    return this.message ?: "Something went wrong"
 }
