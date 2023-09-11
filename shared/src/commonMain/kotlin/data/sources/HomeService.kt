957142bf99cdc2c0094b40f31e02d93b41d1960d
ndiritumichael <ndiritumichael5@gmail.com>
2023-09-11 23:57:10 +0300

    add drink categories functionality

--- shared/src/commonMain/kotlin/data/sources/HomeService.kt
+++ shared/src/commonMain/kotlin/data/sources/HomeService.kt
@@ -7,6 +7,7 @@ import data.network.networkutils.BaseApiResponse
 import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 
 class HomeService(private val client: HttpClient) : BaseApiResponse() {
 
@@ -17,11 +18,16 @@ class HomeService(private val client: HttpClient) : BaseApiResponse() {
     }
 
     suspend fun categoryList(): Result<List<CategoryDto>> {
-        return safeApiCall<List<CategoryDto>> {
+        return safeApiCall {
             client.get(UrlRoutes.ListCategories.path)
-        }.also {
-            Napier.e {
-                "The categories are $it"
+        }
+    }
+
+
+    suspend fun getCategoryDrinks(category : String):Result<List<DrinkDTOItem>>{
+        return safeApiCall{
+            client.get(UrlRoutes.FilterbyCategory.path){
+                parameter(UrlRoutes.FilterbyCategory.queryKey!!,category)
             }
         }
     }
