36d15e3c32ec00559260a15273accca3e9053a80
ndiritumichael <ndiritumichael5@gmail.com>
2023-09-12 23:39:42 +0300

    add functionality to fetch categroy drinks

--- shared/src/commonMain/kotlin/data/sources/HomeService.kt
+++ shared/src/commonMain/kotlin/data/sources/HomeService.kt
@@ -1,10 +1,11 @@
 package data.sources
 
 import data.network.UrlRoutes
+import data.network.dto.drinkDto.CategoryDrinkDTO
 import data.network.dto.drinkDto.CategoryDto
 import data.network.dto.drinkDto.DrinkDTOItem
+import data.network.dto.ingredients.IngredientModel
 import data.network.networkutils.BaseApiResponse
-import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
@@ -12,7 +13,7 @@ import io.ktor.client.request.parameter
 class HomeService(private val client: HttpClient) : BaseApiResponse() {
 
     suspend fun getRandomCockTail(): Result<List<DrinkDTOItem>> {
-        return safeApiCall{
+        return safeApiCall {
             client.get(UrlRoutes.GetRandomCocktail.path)
         }
     }
@@ -23,12 +24,17 @@ class HomeService(private val client: HttpClient) : BaseApiResponse() {
         }
     }
 
-
-    suspend fun getCategoryDrinks(category : String):Result<List<DrinkDTOItem>>{
-        return safeApiCall{
-            client.get(UrlRoutes.FilterbyCategory.path){
-                parameter(UrlRoutes.FilterbyCategory.queryKey!!,category)
+    suspend fun getCategoryDrinks(category: String): Result<List<CategoryDrinkDTO>> {
+        return safeApiCall {
+            client.get(UrlRoutes.FilterbyCategory.path) {
+                parameter(UrlRoutes.FilterbyCategory.queryKey!!, category)
             }
+        }
+    }
+
+    suspend fun getIngredientList(): Result<List<IngredientModel>> {
+        return safeApiCall {
+            client.get(UrlRoutes.ListIngredients.path)
         }
     }
 }
