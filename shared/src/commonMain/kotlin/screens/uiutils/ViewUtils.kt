f319cd36b90b4442db572c2f46df6f83fc6153c9
ndiritumichael <ndiritumichael5@gmail.com>
2023-09-19 23:22:41 +0300

    remove custom image implementation

--- shared/src/commonMain/kotlin/screens/uiutils/ViewUtils.kt
+++ shared/src/commonMain/kotlin/screens/uiutils/ViewUtils.kt
@@ -1,5 +1,5 @@
 package screens.uiutils
 
 fun getIngredientImage(name: String): String {
-    return "www.thecocktaildb.com/images/ingredients/$name-Small.png"
+    return "https://www.thecocktaildb.com/images/ingredients/$name-Small.png"
 }
