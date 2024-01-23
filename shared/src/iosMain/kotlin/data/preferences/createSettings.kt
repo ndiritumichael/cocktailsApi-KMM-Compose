21751a8726a9a435740a885390a9d27fdbf87e4b
ndiritumichael <ndiritumichael5@gmail.com>
2024-01-24 00:22:12 +0300

    minor code cleanup

--- shared/src/iosMain/kotlin/data/preferences/createSettings.kt
+++ shared/src/iosMain/kotlin/data/preferences/createSettings.kt
@@ -2,8 +2,8 @@ package data.preferences
 
 import com.russhwolf.settings.NSUserDefaultsSettings
 import com.russhwolf.settings.ObservableSettings
-import com.russhwolf.settings.Settings
 import platform.Foundation.NSUserDefaults
+
 actual class SettingWrapper {
     actual fun createSettings(): ObservableSettings {
         val delegate = NSUserDefaults.standardUserDefaults
